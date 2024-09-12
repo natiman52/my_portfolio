b5eded74a61843b02dd68950af311e7d7bb028ee
MyICTProject <natnaelyazchew135210@gmail.com>
2024-09-12 11:34:17 +0300

    fix joined path

--- framework/urls.py
+++ framework/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/",include("Api.urls")),
-    re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="index.html")),
 ]
 
