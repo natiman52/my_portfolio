272b3b4fc8018507e26d4514b0bacdb8a345131f
MyICTProject <natnaelyazchew135210@gmail.com>
2024-09-12 16:54:25 +0300

    req

--- framework/settings.py
+++ framework/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wg4lksm!q=oe52(5-pv^1w(=vk*f7(7_hwx5z)&i#ns*r=vr5-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
--- framework/urls.py
+++ framework/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
 ]
-urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
